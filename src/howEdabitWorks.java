8b65a96a647bf4ef81d5ed4698018f7fbabb21a7
christiantc <christiantambo15@gmail.com>
2021-04-05 13:31:35 -0400

    How Edabit Works

---
+++ src/howEdabitWorks.java
@@ -0,0 +1,9 @@
+public class howEdabitWorks {
+    public static void main(String[] args) {
+        System.out.println(returnTrue());
+    }
+
+    public static boolean returnTrue() {
+        return true;
+    }
+}
